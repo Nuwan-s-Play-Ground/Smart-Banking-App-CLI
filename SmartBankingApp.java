47af7c9e160e0ec71495eb0ce7c9a29a54f2b54e
Nuwan Vidanapathirnage <nuwanpvp@gmail.com>
2023-08-16 07:26:38 +0530

    Account balance Calculation steps.

--- SmartBankingApp.java
+++ SmartBankingApp.java
@@ -30,7 +30,8 @@ public class SmartBankingApp{
         String screen = DASHBOARD;
         int i = 1;
 
-        loopOne: do {
+        loopOne: 
+        do {
             final String APP_TITLE = String.format("%s%s%s", COLOR_BLUE_BOLD, screen, RESET);
 
             System.out.println(CLEAR);
@@ -147,7 +148,8 @@ public class SmartBankingApp{
                                 }
                             }
                         }
-                        String initialAccountBalance;
+
+                        
                         loopTwo:
                          do {
                             valid = true;
@@ -158,14 +160,14 @@ public class SmartBankingApp{
                                     if (!Character.isDigit(initialAccountBalance.charAt(j))) {
                                         valid = false;
                                         System.out.printf(ERROR_MSG, "Enter a Valid Amount");
-                                        continue loop2;
+                                        continue loopTwo;
                                     }
                                 }
                                 if (!(initialAccountBalance.charAt((initialAccountBalance.length() - 3)) == '.' || Character
                                         .isDigit(initialAccountBalance.charAt(initialAccountBalance.length() - 3)))) {
                                     valid = false;
                                     System.out.printf(ERROR_MSG, "Enter a Valid Amount ");
-                                    continue loop2;
+                                    continue loopTwo;
     
                                 } else {
                                     for (int j = initialAccountBalance.length() - 2; j < initialAccountBalance
@@ -173,7 +175,7 @@ public class SmartBankingApp{
                                         if (!Character.isDigit(initialAccountBalance.charAt(j))) {
                                             valid = false;
                                             System.out.printf(ERROR_MSG, "Enter a Valid Amount");
-                                            continue loop2;
+                                            continue loopTwo;
                                         }
                                     }
     
@@ -183,10 +185,65 @@ public class SmartBankingApp{
                                     if (!Character.isDigit(initialAccountBalance.charAt(j))) {
                                         valid = false;
                                         System.out.printf(ERROR_MSG, "Enter a Valid Amount");
-                                        continue loop2;
+                                        continue loopTwo;
                                     }
                                 }
                             }
+                            if (valid) {
+                                initialAccountBalanceDouble = Double.parseDouble(initialAccountBalance);
+                                if (initialAccountBalanceDouble < 5000) {
+                                    System.out.printf(ERROR_MSG,
+                                            "Insufficient Initial Deposit Minimum Rs.5000.00 Should be deposited");
+                                    System.out.print("\tDo you Want to Deposit more?(y/n)");
+                                    if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) {
+                                        valid = false;
+                                        continue;
+                                    } else {
+                                        valid=false;
+                                        i--;
+                                        continue loopOne;
+                                    }
     
+                                }
+    
+                            }
+    
+                        } while (!valid);
+
+                        initialAccountBalanceDouble = Double.parseDouble(initialAccountBalance);
+                        System.out.printf("\tDeposited: " + "%s%,.2f", "Rs.", initialAccountBalanceDouble);
+    
+                        String[] newAccountNumbers = new String[accountNumbers.length + 1];
+                        String[] newCustomerNames = new String[customerNames.length + 1];
+                        Double[] newAccountBalances = new Double[accountBalance.length + 1];
+                        for (int j = 0; j < accountNumbers.length; j++) {
+                            newAccountNumbers[j] = accountNumbers[j];
+                            newCustomerNames[j] = customerNames[j];
+                            newAccountBalances[j] = accountBalance[j];
+                        }
+                        newAccountNumbers[newAccountNumbers.length - 1] = id;
+                        newCustomerNames[newAccountNumbers.length - 1] = name;
+                        newAccountBalances[newAccountNumbers.length - 1] = initialAccountBalanceDouble;
+                        accountNumbers = newAccountNumbers;
+                        customerNames = newCustomerNames;
+                        accountBalance = newAccountBalances;
+    
+                        System.out.println();
+                        System.out.printf(SUCCESS_MSG,
+                                String.format("%s:%s has been Created successfully", id, name));
+                        System.out.print("\tDo you want to continue adding (Y/n)? ");
+                        if (SCANNER.nextLine().strip().toUpperCase().equals("Y")) {continue;
+                        screen = DASHBOARD;
+                        break;}
+                        
+                    case DEPOSIT:
+
+            }
+
+        } while (true);
+
     }
+
 }
+    
+    
